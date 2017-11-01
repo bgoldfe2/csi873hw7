dd17001131f52960f97755e7c3c6e240476e37ec
Bruce H. Goldfeder <bgoldfe2@masonlive.gmu.edu>
2017-11-01 18:35:15 -0400

    have code that seems ok, but does nothing

--- gmm.py
+++ gmm.py
@@ -6,92 +6,65 @@ Created on Wed Nov  1 12:33:56 2017
 """
 
 import numpy as np
+import math
     
 class GMM:
     
-    def __init__(self, eps = 0.0001,mi = 1000):
+    def __init__(self, sigma =1,eps = 0.0001,mi = 1000):
         
+        self.sigma = sigma
         self.eps = eps ## threshold to stop `epsilon`
         self.max_iters = mi
-        self.sigma = 1
+        self.sigma = 1 # sigma given as 1 in this hw.
+        self.k = 2 # only two gaussian in this hw.
     
     
     def fit_EM(self, X, max_iters = 1000):
         
         # n = number of data-points       
-        n = X.shape
-                
-        # choose the starting centroids/means 
-        print("k is ", self.k)
-        init_mus = [2,4]
-        mu = np.asarray(init_mus)
-        
+        n = X.shape[0]
+        print("n is ",n)
+    
         #### Continue with steps one and two from text
+        # Excpectations array for step one
+        E = np.zeros((n,self.k))
         
-        # initialize the probabilities/weights for each gaussians
-        w = [1./self.k] * self.k
+        # choose the starting centroids/means         
+        mu = np.divide(np.ones(2),2) 
+        print('mu is ',mu)
         
-        # responsibility matrix is initialized to all zeros
-        # we have responsibility for each of n points for eack of k gaussians
-        R = np.zeros((n, self.k))
-        
-        ### log_likelihoods
-        log_likelihoods = []
-        
-        P = lambda mu, s: np.linalg.det(s) ** -.5 ** (2 * np.pi) ** (-X.shape[1]/2.) \
-                * np.exp(-.5 * np.einsum('ij, ij -> i',\
-                        X - mu, np.dot(np.linalg.inv(s) , (X - mu).T).T ) ) 
-                        
+ 
+        num_iter = 0
         # Iterate till max_iters iterations        
-        while len(log_likelihoods) < max_iters:
+        while num_iter < max_iters:
+            num_iter += 1
+            # Step one find expectation probability the x_i was generated
+            # by the jth Normal distribution
+            for j in range(self.k):
+                for i in range(n):
+                    
+                    E[i,j] = math.exp((-1/(2*sigma**2))*((X[i]-mu[j])**2)) \
+                        / (math.exp((-1/(2*sigma**2))*((X[i]-mu[0])**2)) \
+                        + math.exp((-1/(2*sigma**2))*((X[i]-mu[1])**2))) 
             
-            # E - Step
-            
-            ## Vectorized implementation of e-step equation to calculate the 
-            ## membership for each of k -gaussians
-            for k in range(self.k):
-                R[:, k] = w[k] * P(mu[k], Sigma[k])
-
-            ### Likelihood computation
-            log_likelihood = np.sum(np.log(np.sum(R, axis = 1)))
-            
-            log_likelihoods.append(log_likelihood)
-            
-            ## Normalize so that the responsibility matrix is row stochastic
-            R = (R.T / np.sum(R, axis = 1)).T
-            
-            ## The number of datapoints belonging to each gaussian            
-            N_ks = np.sum(R, axis = 0)
-            
-            
-            # M Step
-            ## calculate the new mean and covariance for each gaussian by 
-            ## utilizing the new responsibilities
-            for k in range(self.k):
+            # Step two derive a new max liklihood hypothesis m_1,m_2
+            for j in range(self.k):
+                numerator = 0.0
+                denominator = 0.0
+                for i in range(n):
+                    numerator += E[i,j] * X[i]
+                    denominator += E[i,j]
+                mu[j]=numerator/denominator
                 
-                ## means
-                mu[k] = 1. / N_ks[k] * np.sum(R[:, k] * X.T, axis = 1).T
-                x_mu = np.matrix(X - mu[k])
-                
-                ## covariances
-                Sigma[k] = np.array(1 / N_ks[k] * np.dot(np.multiply(x_mu.T,  R[:, k]), x_mu))
-                
-                ## and finally the probabilities
-                w[k] = 1. / n * N_ks[k]
-            # check for onvergence
-            if len(log_likelihoods) < 2 : continue
-            if np.abs(log_likelihood - log_likelihoods[-2]) < self.eps: break
+            sum_min = [0.0,0.0]
+            for j in range(self.k):
+                    for i in range(n):
+                        sum_min[j] += (X[i]-mu[j])**2
+            print ("Error squared is ",sum_min)
+            # End of while iteration loop
+            
         
-        ## bind all results together
-        from collections import namedtuple
-        self.params = namedtuple('params', ['mu', 'Sigma', 'w', 'log_likelihoods', 'num_iters'])
-        self.params.mu = mu
-        self.params.Sigma = Sigma
-        self.params.w = w
-        self.params.log_likelihoods = log_likelihoods
-        self.params.num_iters = len(log_likelihoods)       
-        
-        return self.params
+        return mu,sum_min
     
     def plot_log_likelihood(self):
         import pylab as plt
@@ -100,14 +73,6 @@ class GMM:
         plt.xlabel('Iterations')
         plt.ylabel('log likelihood')
         plt.show()
-    
-    def predict(self, x):
-        p = lambda mu, s : np.linalg.det(s) ** - 0.5 * (2 * np.pi) **\
-                (-len(x)/2) * np.exp( -0.5 * np.dot(x - mu , \
-                        np.dot(np.linalg.inv(s) , x - mu)))
-        probs = np.array([w * p(mu, s) for mu, s, w in \
-            zip(self.params.mu, self.params.Sigma, self.params.w)])
-        return probs/np.sum(probs)
         
         
 if __name__ == "__main__":
@@ -119,6 +84,8 @@ if __name__ == "__main__":
     parser.add_option("-f", "--file", dest="filepath", help="File path for data")
     parser.add_option("-e", "--eps", dest="epsilon", help="Epsilon to stop")    
     parser.add_option("-m", "--maxiters", dest="max_iters", help="Maximum no. of iteration")        
+    parser.add_option("-s", "--sigma", dest="sigma", help="Sigma value")        
+
     options, args = parser.parse_args()
     
     if not options.filepath : raise('File not provided')
@@ -133,9 +100,14 @@ if __name__ == "__main__":
         max_iters = 1000
     else: max_iters = int(options.maxiters)
     
+    if not options.sigma :
+        print("Used default sigma = 1" )
+        sigma = 1
+    else: sigma = int(options.sigma)
+    
     X = np.genfromtxt(options.filepath, delimiter=' ')
-    gmm = GMM(eps,max_iters)
-    params = gmm.fit_EM(X, max_iters)
-    print (params.log_likelihoods)
-    gmm.plot_log_likelihood()
-    print (gmm.predict(np.array([1, 2])))
+    gmm = GMM(sigma,eps,max_iters)
+    mu,err = gmm.fit_EM(X, max_iters)
+    print ("mu is ",mu,"error is ",err)
+    #gmm.plot_log_likelihood()
+    
