ba2d33f4443634e6fdabf742980b271777e4dab9
Bruce H. Goldfeder <bgoldfe2@masonlive.gmu.edu>
2017-11-01 19:43:20 -0400

    wow I think I did it

--- gmm.py
+++ gmm.py
@@ -10,12 +10,12 @@ import math
     
 class GMM:
     
-    def __init__(self, sigma =1,eps = 0.0001,mi = 1000):
+    def __init__(self, sigma,eps,mi):
         
         self.sigma = sigma
         self.eps = eps ## threshold to stop `epsilon`
         self.max_iters = mi
-        self.sigma = 1 # sigma given as 1 in this hw.
+        self.sigma = sigma # sigma given as 1 in this hw.
         self.k = 2 # only two gaussian in this hw.
     
     
@@ -30,11 +30,16 @@ class GMM:
         E = np.zeros((n,self.k))
         
         # choose the starting centroids/means         
-        mu = np.divide(np.ones(2),2) 
+        mu = np.asarray([-2.0,-4.0])
         print('mu is ',mu)
         
- 
+        # counter for the number of iterations
         num_iter = 0
+        
+        # difference in error for successive iterations
+        past_err = [5,10]
+       
+        
         # Iterate till max_iters iterations        
         while num_iter < max_iters:
             num_iter += 1
@@ -42,11 +47,16 @@ class GMM:
             # by the jth Normal distribution
             for j in range(self.k):
                 for i in range(n):
+                    #print ("muusss ",mu[0]," - ",mu[1])
+                    #print ("X is ",X[i])
+                    numer = math.exp((-1/(2*self.sigma**2))*((X[i]-mu[j])**2))
+                    denom1 = math.exp((-1/(2*self.sigma**2))*((X[i]-mu[0])**2)) 
+                    denom2 = math.exp((-1/(2*self.sigma**2))*((X[i]-mu[1])**2))
+                    denom = denom1 + denom2
+                    #print ("numer is ", numer, " denom1 is ",denom1," denom2 is ",denom2, "denom is ",denom)
+                    E[i,j] = numer/denom
+                    #print(E[i,j],end="")
                     
-                    E[i,j] = math.exp((-1/(2*sigma**2))*((X[i]-mu[j])**2)) \
-                        / (math.exp((-1/(2*sigma**2))*((X[i]-mu[0])**2)) \
-                        + math.exp((-1/(2*sigma**2))*((X[i]-mu[1])**2))) 
-            
             # Step two derive a new max liklihood hypothesis m_1,m_2
             for j in range(self.k):
                 numerator = 0.0
@@ -56,23 +66,32 @@ class GMM:
                     denominator += E[i,j]
                 mu[j]=numerator/denominator
                 
+                
             sum_min = [0.0,0.0]
             for j in range(self.k):
                     for i in range(n):
                         sum_min[j] += (X[i]-mu[j])**2
             print ("Error squared is ",sum_min)
+            if ((sum_min[0]-past_err[0]) + (sum_min[1]-past_err[1])) < 2*eps:
+                break
             # End of while iteration loop
             
         
         return mu,sum_min
     
-    def plot_log_likelihood(self):
-        import pylab as plt
-        plt.plot(self.params.log_likelihoods)
-        plt.title('Log Likelihood vs iteration plot')
-        plt.xlabel('Iterations')
-        plt.ylabel('log likelihood')
-        plt.show()
+    def plot_normals(self,mu):
+       import matplotlib.pyplot as plt
+       import matplotlib.mlab as mlab
+       
+        
+       
+       x1 = np.linspace(mu[0] - 3*self.sigma, mu[0] + 3*self.sigma, 100)
+       plt.plot(x1,mlab.normpdf(x1, mu[0], self.sigma))
+       
+       x2 = np.linspace(mu[0] - 3*self.sigma, mu[1] + 3*self.sigma, 100)
+       plt.plot(x2,mlab.normpdf(x2, mu[1], self.sigma))
+
+       plt.show()
         
         
 if __name__ == "__main__":
@@ -97,7 +116,7 @@ if __name__ == "__main__":
     
     if not options.max_iters :
         print("Used default maxiters = 1000" )
-        max_iters = 1000
+        max_iters = 200
     else: max_iters = int(options.maxiters)
     
     if not options.sigma :
@@ -108,6 +127,6 @@ if __name__ == "__main__":
     X = np.genfromtxt(options.filepath, delimiter=' ')
     gmm = GMM(sigma,eps,max_iters)
     mu,err = gmm.fit_EM(X, max_iters)
-    print ("mu is ",mu,"error is ",err)
-    #gmm.plot_log_likelihood()
+    print ("mu is ",mu)
+    gmm.plot_normals(mu)
     
